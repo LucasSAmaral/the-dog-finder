56d729d41292bf16b27521c60db0d1997fce7d67
Lucas <lucassantosamaral@hotmail.com>
2018-11-15 00:30:58 -0200

    Criando as paginas Main e Racas e ajustando o router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/pages/main'
+import Racas from '@/pages/racas'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/racas',
+      name: 'Racas',
+      component: Racas
     }
   ]
 })
